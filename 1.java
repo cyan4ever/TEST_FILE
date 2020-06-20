997685a199a38af38c62a2a7c755a8a84f5ab5f1
Aiweilai <totwahaha@sina.com>
2020-06-20 11:11:52 +0800

    java

---
+++ 1.java
@@ -0,0 +1,12 @@
+class Test //第一个写的小程序，舒服了！
+{
+          public static void main(String[] args)
+          {
+                     System.out.println("hello NEW WORLD");
+		System.out.println("my mom is beautiful");
+          }
+}
+/*
+靡不有初
+鲜克有终
+*/
